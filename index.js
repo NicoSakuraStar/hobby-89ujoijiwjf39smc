b6ea530264ec21c2f5a550e227b70a60af6a4df4
NicoSakuraStar <niconiconi463@gmail.com>
2022-06-04 17:59:38 +0200

    Implemented more stuff that hopefully makes tailwind work

---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import './styles.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
--- script.js
+++
@@ -1,7 +0,0 @@
-console.log('Not sure what to write here...')
-
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
